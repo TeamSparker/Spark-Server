799ea618d42302a375f9359a6104661ae7a3bb1e
youngkwon02 <youngkwon02@naver.com>
2022-01-09 17:15:40 +0900

    style: router structure

--- functions/api/routes/index.js
+++ functions/api/routes/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-// '/user' 이하의 경로로 들어온 요청은 모두 user 폴더 안에서 처리
+router.use('/auth', require('./auth'));
+router.use('/feed', require('./feed'));
+router.use('/myroom', require('./myroom'));
+router.use('/notice', require('./notice'));
+router.use('/room', require('./room'));
 router.use('/user', require('./user'));
 
 module.exports = router;
---
+++ functions/api/routes/myroom/index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const router = express.Router();
+
+// router.post('/signup', require('./userSignupPOST'));
+
+module.exports = router;
