33758b903786b6d8f941632555f2b39e088cc694
youngkwon02 <youngkwon02@naver.com>
2022-01-10 16:10:30 +0900

    feat: userDB getAllUsers, getUserById

--- functions/db/user.js
+++ functions/db/user.js
@@ -4,7 +4,7 @@ const convertSnakeToCamel = require('../lib/convertSnakeToCamel');
 const getAllUsers = async (client) => {
   const { rows } = await client.query(
     `
-    SELECT * FROM "user" u
+    SELECT * FROM spark.user as u
     WHERE is_deleted = FALSE
     `,
   );
@@ -14,8 +14,8 @@ const getAllUsers = async (client) => {
 const getUserById = async (client, userId) => {
   const { rows } = await client.query(
     `
-    SELECT * FROM "user" u
-    WHERE id = $1
+    SELECT * FROM spark.user as u
+    WHERE user_id = $1
       AND is_deleted = FALSE
     `,
     // client.query()의 두 번째 파라미터에는, 쿼리문에 집어넣고 싶은 변수들의 배열을 적습니다.
@@ -26,83 +26,4 @@ const getUserById = async (client, userId) => {
   return convertSnakeToCamel.keysToCamel(rows[0]);
 };
 
-const getUserByIdFirebase = async (client, idFirebase) => {
-  const { rows } = await client.query(
-    `
-    SELECT * FROM "user" u
-    WHERE id_firebase = $1
-      AND is_deleted = FALSE
-    `,
-    [idFirebase],
-  );
-  return convertSnakeToCamel.keysToCamel(rows[0]);
-};
-
-const getUserByEmail = async (client, email) => {
-  const { rows } = await client.query(
-    `
-    SELECT * FROM "user" u
-    WHERE email = $1
-      AND is_deleted = FALSE
-    `,
-    [email],
-  );
-  return convertSnakeToCamel.keysToCamel(rows[0]);
-};
-
-const updateUser = async (client, username, phone, userId) => {
-  const { rows: existingRows } = await client.query(
-    `
-    SELECT * FROM "user"
-    WHERE id = $1
-       AND is_deleted = FALSE
-    `,
-    [userId],
-  );
-
-  if (existingRows.length === 0) return false;
-
-  const data = _.merge({}, convertSnakeToCamel.keysToCamel(existingRows[0]), { username, phone });
-
-  const { rows } = await client.query(
-    `
-    UPDATE "user" u
-    SET username = $1, phone = $2, updated_at = now()
-    WHERE id = $3
-    RETURNING * 
-    `,
-    [data.username, data.phone, userId],
-  );
-  return convertSnakeToCamel.keysToCamel(rows[0]);
-};
-
-const deleteUser = async (client, userId) => {
-  const { rows } = await client.query(
-    `
-    UPDATE "user" u
-    SET is_deleted = TRUE, updated_at = now()
-    WHERE id = $1
-    RETURNING *
-    `,
-    [userId],
-  );
-
-  return convertSnakeToCamel.keysToCamel(rows[0]);
-};
-
-const addUser = async (client, email, username, phone, idFirebase) => {
-  const { rows } = await client.query(
-    `
-    INSERT INTO "user"
-    (email, username, phone, id_firebase)
-    VALUES
-    ($1, $2, $3, $4)
-    RETURNING *
-    `,
-
-    [email, username, phone, idFirebase],
-  );
-  return convertSnakeToCamel.keysToCamel(rows[0]);
-};
-
-module.exports = { getAllUsers, getUserById, getUserByIdFirebase, getUserByEmail, updateUser, deleteUser, addUser };
+module.exports = { getAllUsers, getUserById };
