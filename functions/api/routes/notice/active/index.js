9ab0c4743397f60f7bf155e4373da251455b8a40
youngkwon02 <youngkwon02@naver.com>
2022-01-11 18:44:44 +0900

    fix: notification router relative path error

--- functions/api/routes/notice/active/index.js
+++ functions/api/routes/notice/active/index.js
@@ -1,8 +1,4 @@
 const express = require('express');
 const router = express.Router();
-const { checkUser } = require('../../../middlewares/auth');
-
-router.post('', require('./roomPOST'));
-router.get('/code/:code', checkUser, require('./roomCodeGET'));
 
 module.exports = router;
--- functions/api/routes/notice/service/index.js
+++ functions/api/routes/notice/service/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const { checkUser } = require('../../../middlewares/auth');
+const { checkUser } = require('../../../../middlewares/auth');
 
-router.post('/read', require('./serviceReadPATCH'));
+router.patch('/read', checkUser, require('./serviceReadPATCH'));
 
 module.exports = router;
