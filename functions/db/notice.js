e5be423b35655118d3a9c2e20b73e7a3efe978e7
youngkwon02 <youngkwon02@naver.com>
2022-01-11 19:46:24 +0900

    feat: 활동 알림 읽음처리 api

--- functions/db/notice.js
+++ functions/db/notice.js
@@ -17,4 +17,20 @@ const serviceReadByUserId = async (client, userId) => {
   return convertSnakeToCamel.keysToCamel(rows);
 };
 
-module.exports = { serviceReadByUserId };
+const activeReadByUserId = async (client, userId) => {
+  const { rows } = await client.query(
+    `
+    UPDATE spark.notification
+    SET is_read = TRUE, read_at = now(), updated_at = now()
+    WHERE is_read = FALSE 
+    AND is_deleted = FALSE 
+    AND is_service = FALSE 
+    AND receiver_id = $1
+    RETURNING *
+    `,
+    [userId],
+  );
+  return convertSnakeToCamel.keysToCamel(rows);
+};
+
+module.exports = { serviceReadByUserId, activeReadByUserId };
