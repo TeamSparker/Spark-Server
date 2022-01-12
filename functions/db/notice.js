9f825cbbc7d314980233adf47a91efec072e6738
youngkwon02 <youngkwon02@naver.com>
2022-01-12 16:42:09 +0900

    refactor: 알림 관련 API time-log timezone (utc+9)

--- functions/db/notice.js
+++ functions/db/notice.js
@@ -1,39 +1,43 @@
+const dayjs = require('dayjs');
 const _ = require('lodash');
 const convertSnakeToCamel = require('../lib/convertSnakeToCamel');
 
 const serviceReadByUserId = async (client, userId) => {
+  const now = dayjs().add(9, 'hour');
   const { rows } = await client.query(
     `
     UPDATE spark.notification
-    SET is_read = TRUE, read_at = now(), updated_at = now()
+    SET is_read = TRUE, read_at = $2, updated_at = $2
     WHERE is_read = FALSE 
     AND is_deleted = FALSE 
     AND is_service = TRUE 
     AND receiver_id = $1
     RETURNING *
     `,
-    [userId],
+    [userId, now],
   );
   return convertSnakeToCamel.keysToCamel(rows);
 };
 
 const activeReadByUserId = async (client, userId) => {
+  const now = dayjs().add(9, 'hour');
   const { rows } = await client.query(
     `
     UPDATE spark.notification
-    SET is_read = TRUE, read_at = now(), updated_at = now()
+    SET is_read = TRUE, read_at = $2, updated_at = $2
     WHERE is_read = FALSE 
     AND is_deleted = FALSE 
     AND is_service = FALSE 
     AND receiver_id = $1
     RETURNING *
     `,
-    [userId],
+    [userId, now],
   );
   return convertSnakeToCamel.keysToCamel(rows);
 };
 
 const getServicesByUserId = async (client, userId, lastid, size) => {
+  const beforeAWeek = dayjs().subtract(7, 'day');
   const { rows } = await client.query(
     `
       SELECT * FROM spark.notification
@@ -41,16 +45,17 @@ const getServicesByUserId = async (client, userId, lastid, size) => {
       AND is_deleted = FALSE
       AND is_service = TRUE
       AND notification_id < $2
-      AND created_at > current_timestamp + '-7 days'
+      AND created_at > $4
       ORDER BY notification_id DESC
       LIMIT $3
     `,
-    [userId, lastid, size],
+    [userId, lastid, size, beforeAWeek],
   );
   return convertSnakeToCamel.keysToCamel(rows);
 };
 
 const getActivesByUserId = async (client, userId, lastid, size) => {
+  const beforeAWeek = dayjs().subtract(7, 'day');
   const { rows } = await client.query(
     `
       SELECT * FROM spark.notification
@@ -58,11 +63,11 @@ const getActivesByUserId = async (client, userId, lastid, size) => {
       AND is_deleted = FALSE
       AND is_service = FALSE
       AND notification_id < $2
-      AND created_at > current_timestamp + '-7 days'
+      AND created_at > $4
       ORDER BY notification_id DESC
       LIMIT $3
     `,
-    [userId, lastid, size],
+    [userId, lastid, size, beforeAWeek],
   );
   return convertSnakeToCamel.keysToCamel(rows);
 };
