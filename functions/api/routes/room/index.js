7284f69e967282ccb019c8acee5aff8d37cb036a
youngkwon02 <youngkwon02@naver.com>
2022-01-13 16:26:15 +0900

    feat: roomWaitingGET router connect

--- functions/api/routes/room/index.js
+++ functions/api/routes/room/index.js
@@ -5,5 +5,6 @@ const { checkUser } = require('../../../middlewares/auth');
 router.get('/code/:code', checkUser, require('./roomCodeGET'));
 router.post('', checkUser, require('./roomPOST'));
 router.patch('/:roomId/purpose', checkUser, require('./roomPurposePATCH'));
+router.get('/:roomId/waiting', checkUser, require('roomWaitingGET'));
 
 module.exports = router;
