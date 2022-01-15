625a80219201c65693f702f5f6dc8517b7d6071f
youngkwon02 <youngkwon02@naver.com>
2022-01-15 19:54:07 +0900

    style: create alarmMessage const file

--- functions/constants/responseMessage.js
+++ functions/constants/responseMessage.js
@@ -41,13 +41,12 @@ module.exports = {
   NOT_ONGOING_ROOM: '현재 진행중인 습관방이 아닙니다',
   NOT_MEMBER: '참여중인 습관방이 아닙니다',
   GET_ROOM_LIST_SUCCESS: '참여중인 습관방 조회 완료',
-    
+
   START_ROOM_SUCCESS: '습관 방 시작 완료',
   START_ROOM_ALREADY: '이미 시작된 방입니다',
 
   //Feed
   GET_FEED_SUCCES: '피드 조회 성공',
-
 
   // Notice
   SERVICE_READ_SUCCESS: '서비스 알림 읽음처리 완료',
