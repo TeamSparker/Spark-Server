16903fd0108f9806e6a7436e2033aa577dcb2414
youngkwon02 <youngkwon02@naver.com>
2022-01-17 01:16:44 +0900

    feat: 습관 방 시작시 알림 및 푸시알림

--- functions/api/routes/room/roomStartPOST.js
+++ functions/api/routes/room/roomStartPOST.js
@@ -1,9 +1,11 @@
 const functions = require('firebase-functions');
 const util = require('../../../lib/util');
 const statusCode = require('../../../constants/statusCode');
+const alarmMessage = require('../../../constants/alarmMessage');
 const responseMessage = require('../../../constants/responseMessage');
 const db = require('../../../db/db');
-const { roomDB, recordDB } = require('../../../db');
+const pushAlarm = require('../../../lib/pushAlarm');
+const { userDB, roomDB, recordDB, noticeDB } = require('../../../db');
 
 /**
  *  @습관방_시작
@@ -51,16 +53,24 @@ module.exports = async (req, res) => {
       return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.START_ROOM_ALREADY));
     }
 
-    // 참여자들의 0일차 record 생성
     const entries = await roomDB.getEntriesByRoomId(client, roomId);
+    const { title, body, isService } = alarmMessage.ROOM_NEW(room.roomName);
 
     for (let i = 0; i < entries.length; i++) {
-      await recordDB.insertRecordById(client, entries[i].entryId, room.startAt);
+      const entry = entries[i];
+      const targetId = entry.userId;
+      const target = await userDB.getUserById(client, targetId);
+      const receiverToken = target.deviceToken;
+
+      // 참여자들의 0일차 record 생성
+      await recordDB.insertRecordById(client, entry.entryId, room.startAt);
+
+      // 방이 시작되면, 참여자들에게 알림 및 푸시알림 보내기
+      await noticeDB.addNotification(client, title, body, 'Spark_IMG_URL', targetId, isService);
+      pushAlarm.send(req, res, receiverToken, 'Spark', body);
     }
 
-    // @TODO: Send Notification !!
-
-    res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.START_ROOM_SUCCESS));
+    return res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.START_ROOM_SUCCESS));
   } catch (error) {
     functions.logger.error(`[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`, `[CONTENT] ${error}`);
     console.log(error);
