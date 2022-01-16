b680881f9b566bba37757d163abea2a0b70fc074
youngkwon02 <youngkwon02@naver.com>
2022-01-16 16:51:27 +0900

    fix: duplicated response

--- functions/api/routes/room/roomStatusPOST.js
+++ functions/api/routes/room/roomStatusPOST.js
@@ -73,12 +73,12 @@ module.exports = async (req, res) => {
       }
     }
 
-    res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.UPDATE_STATUS_SUCCESS));
+    return res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.UPDATE_STATUS_SUCCESS));
   } catch (error) {
     functions.logger.error(`[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`, `[CONTENT] ${error}`);
     console.log(error);
 
-    res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR));
+    // res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR));
   } finally {
     client.release();
   }
