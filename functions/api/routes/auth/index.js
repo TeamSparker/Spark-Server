d13d73156274d5afe392631e6026dd85c0e4e1aa
youngkwon02 <youngkwon02@naver.com>
2022-01-16 20:36:39 +0900

    fix: annotation of auth router

--- functions/api/routes/auth/index.js
+++ functions/api/routes/auth/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const uploadImage = require('../../../middlewares/uploadImage');
 const { checkUser } = require('../../../middlewares/auth');
 
-router.post('/signup',uploadImage, require('./authSignupPOST'));
-router.get('/test', checkUser, require('./authTestGET'));
+// router.post('/signup',uploadImage, require('./authSignupPOST'));
+// router.get('/test', checkUser, require('./authTestGET'));
 
 module.exports = router;
