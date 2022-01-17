a691d67ed489cdc036e75f493a75153e43c0039b
xxeol <jhj01177@naver.com>
2022-01-18 00:45:46 +0900

    style: 불필요한 코드 삭제

--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,7 @@
 const admin = require('firebase-admin');
 const serviceAccount = require('./we-sopt-spark-firebase-adminsdk-emnjd-30d5170309.json');
 const dotenv = require('dotenv');
-const dayjs = require('dayjs');
 const scheduler = require('./scheduler/scheduler');
-const schedule = require('node-schedule');
-
-// const job = schedule.scheduleJob('1,5,11,15,21,25,31,35,41,45,51,55 * * * * *', function() {
-//   console.log(dayjs());  
-//   console.log("hi");
-// });
 
 dotenv.config();
 
