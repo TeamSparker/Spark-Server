2f1e25c5629c935a6400a13d472605fe2d13132f
xxeol <jhj01177@naver.com>
2022-01-18 00:39:53 +0900

    feat: schedule time 변경

--- functions/scheduler/scheduler.js
+++ functions/scheduler/scheduler.js
@@ -2,10 +2,8 @@
 const schedule = require('node-schedule');
 const funcs = require('./funcs.js');
 
-const job = schedule.scheduleJob('0,10,20,30,40,50 * * * * *', function() {
-    // console.log("hi");
+const job = schedule.scheduleJob('* 0 * * *', function() {
     funcs.checkLife();
-    // funcs.addRecords();
 });
 
 module.exports= {
