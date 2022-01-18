e67ea52bb87a617de709fb513e5b9da4f9d5f4b2
youngkwon02 <youngkwon02@naver.com>
2022-01-19 05:10:49 +0900

    feat: Slack API - roomPOST

--- functions/api/routes/room/roomPOST.js
+++ functions/api/routes/room/roomPOST.js
@@ -3,6 +3,7 @@ const util = require('../../../lib/util');
 const statusCode = require('../../../constants/statusCode');
 const responseMessage = require('../../../constants/responseMessage');
 const db = require('../../../db/db');
+const slackAPI = require('../../../middlewares/slackAPI');
 const { roomDB } = require('../../../db');
 const { nanoid } = require('nanoid');
 
@@ -47,6 +48,8 @@ module.exports = async (req, res) => {
   } catch (error) {
     functions.logger.error(`[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`, `[CONTENT] ${error}`);
     console.log(error);
+    const slackMessage = `[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl} ${error} ${JSON.stringify(error)}`;
+    slackAPI.sendMessageToSlack(slackMessage, slackAPI.DEV_WEB_HOOK_ERROR_MONITORING);
 
     res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR));
   } finally {
