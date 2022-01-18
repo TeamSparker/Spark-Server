17b8c0de8df6b9b2c9adf929b2aa37a53f33f808
xxeol <jhj01177@naver.com>
2022-01-19 00:17:13 +0900

    feat: 폭파된 방 아이디, 살아남은 방 아이디 슬랙 연동

--- functions/scheduler/funcs.js
+++ functions/scheduler/funcs.js
@@ -46,6 +46,8 @@ const checkLife = async() => {
         });
         const resultRecords = await recordDB.insertRecords(client, insertEntries); // record 추가!
         console.log(resultRecords);
+        const slackMessage = `폭파된 방 목록: ${failRoomIds} / 살아남은 방 목록: ${successRoomIds}`;
+        slackAPI.sendMessageToSlack(slackMessage, slackAPI.DEV_WEB_HOOK_ERROR_MONITORING);
       } catch (error) {
         const slackMessage = `[ERROR] ${error} ${JSON.stringify(error)}`;
         slackAPI.sendMessageToSlack(slackMessage, slackAPI.DEV_WEB_HOOK_ERROR_MONITORING);
