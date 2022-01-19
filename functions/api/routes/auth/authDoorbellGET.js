25d728a94b8170720aa3a27e732d682dc0cb4b2f
youngkwon02 <youngkwon02@naver.com>
2022-01-19 21:29:25 +0900

    feat: Slack API - authDoorbellGET

--- functions/api/routes/auth/authDoorbellGET.js
+++ functions/api/routes/auth/authDoorbellGET.js
@@ -4,6 +4,7 @@ const statusCode = require('../../../constants/statusCode');
 const responseMessage = require('../../../constants/responseMessage');
 const db = require('../../../db/db');
 const jwtHandlers = require('../../../lib/jwtHandlers');
+const slackAPI = require('../../../middlewares/slackAPI');
 const { userDB } = require('../../../db');
 
 /**
@@ -38,6 +39,8 @@ module.exports = async (req, res) => {
   } catch (error) {
     console.log(error);
     functions.logger.error(`[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`, `[CONTENT] ${error}`);
+    const slackMessage = `[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl} ${error} ${JSON.stringify(error)}`;
+    slackAPI.sendMessageToSlack(slackMessage, slackAPI.DEV_WEB_HOOK_ERROR_MONITORING);
 
     res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR));
   } finally {
