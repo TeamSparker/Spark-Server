53ab855ddea97d5a2b9335c65dec3a655b1a4763
youngkwon02 <youngkwon02@naver.com>
2022-01-19 20:16:59 +0900

    feat: Slack API - roomWaitingMemberGET

--- functions/api/routes/room/roomWaitingMemberGET.js
+++ functions/api/routes/room/roomWaitingMemberGET.js
@@ -3,6 +3,7 @@ const util = require('../../../lib/util');
 const statusCode = require('../../../constants/statusCode');
 const responseMessage = require('../../../constants/responseMessage');
 const db = require('../../../db/db');
+const slackAPI = require('../../../middlewares/slackAPI');
 const { userDB, roomDB } = require('../../../db');
 
 /**
@@ -39,7 +40,7 @@ module.exports = async (req, res) => {
 
     // 요청을 보낸 사용자를 제외한 member list
     const friendsEntries = await roomDB.getFriendsByIds(client, roomId, userId);
-    if(!friendsEntries.length) {
+    if (!friendsEntries.length) {
       res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.GET_WAITROOM_DATA_SUCCESS, { members: [] }));
     }
     const friendsIds = friendsEntries.map((f) => f.userId);
@@ -58,6 +59,9 @@ module.exports = async (req, res) => {
   } catch (error) {
     functions.logger.error(`[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`, `[CONTENT] ${error}`);
     console.log(error);
+    const slackMessage = `[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl} ${error} ${JSON.stringify(error)}`;
+    slackAPI.sendMessageToSlack(slackMessage, slackAPI.DEV_WEB_HOOK_ERROR_MONITORING);
+
     res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR));
   } finally {
     client.release();
