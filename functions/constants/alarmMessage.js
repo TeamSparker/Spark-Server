0c931fe4a69cd7ef62cfea9033085878b9b689d3
youngkwon02 <youngkwon02@naver.com>
2022-01-21 03:07:57 +0900

    style: 스파크 보내기 Notice title 수정

--- functions/constants/alarmMessage.js
+++ functions/constants/alarmMessage.js
@@ -20,7 +20,7 @@ const ROOM_OUT = (who, roomName) => {
 };
 
 const SEND_SPARK = (who, body) => {
-  const title = `${who}님이 보낸 스파크🔥`;
+  const title = `${who}님이 보낸 스파크`;
 
   return { title, body, isService: false };
 };
