8f63602364b65a360281cf9a2c5c0b8f79c4f2ca
youngkwon02 <youngkwon02@naver.com>
2022-02-08 17:59:22 +0900

    feat: userProfilePATCH 프로필 변경 API

--- functions/api/routes/user/index.js
+++ functions/api/routes/user/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const { checkUser } = require('../../../middlewares/auth');
+const uploadImageIntoSubDir = require('../../../middlewares/uploadImage');
 
 router.get('/profile', checkUser, require('./userProfileGET'));
+router.patch('/profile', checkUser, uploadImageIntoSubDir('users'), require('./userProfilePATCH'));
 
 module.exports = router;
