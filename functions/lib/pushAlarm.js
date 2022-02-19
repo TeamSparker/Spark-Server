a224f0663547906d5534e5b99728a8c5d090337c
youngkwon02 <youngkwon02@naver.com>
2022-02-20 02:20:34 +0900

    feat: sendMulticastByTokens with imageUrl

--- functions/lib/pushAlarm.js
+++ functions/lib/pushAlarm.js
@@ -3,16 +3,9 @@ const functions = require('firebase-functions');
 const util = require('./util');
 const statusCode = require('../constants/statusCode');
 const responseMessage = require('../constants/responseMessage');
-const db = require('../db/db');
 
 const send = async (req, res, receiverToken, title, body) => {
-  let client;
-
   try {
-    client = await db.connect(req);
-
-    const deviceToken = receiverToken;
-
     const message = {
       android: {
         data: {
@@ -30,30 +23,70 @@ const send = async (req, res, receiverToken, title, body) => {
           },
         },
       },
-      token: deviceToken,
+      token: receiverToken,
     };
 
     admin
       .messaging()
       .send(message)
       .then(function (response) {
-        console.log('Successfully sent message: : ', response);
         return true;
       })
       .catch(function (err) {
-        console.log('Error Sending message!!! : ', err);
         return res.status(400).json({ success: false });
       });
     return true;
   } catch (error) {
     functions.logger.error(`[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`, `[CONTENT] ${error}`);
-    console.log(error);
     return res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR));
   } finally {
-    client.release();
+  }
+};
+
+const sendMulticastByTokens = async (req, res, title, body, receiverTokens, imageUrl = '', expand = 'enable') => {
+  try {
+    const message = {
+      android: {
+        data: {
+          title,
+          body,
+          imageUrl,
+          expand,
+        },
+      },
+      apns: {
+        payload: {
+          aps: {
+            alert: {
+              title,
+              body,
+              imageUrl,
+              expand,
+            },
+          },
+        },
+      },
+      tokens: receiverTokens,
+    };
+
+    admin
+      .messaging()
+      .sendMulticast(message)
+      .then(function (response) {
+        return true;
+      })
+      .catch(function (err) {
+        return res.status(400).json({ success: false });
+      });
+    return true;
+  } catch (error) {
+    functions.logger.error(`[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`, `[CONTENT] ${error}`);
+    return res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR));
+  } finally {
   }
 };
 
 module.exports = {
   send,
+  sendMulticastByTokens,
 };
