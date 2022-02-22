596ea2e81bc7e5df46aadb1906738d5ee6618dd6
youngkwon02 <youngkwon02@naver.com>
2022-02-22 14:02:41 +0900

    refactor: 대기방 및 습관방 나가기 API Request method 변경

--- functions/api/routes/room/index.js
+++ functions/api/routes/room/index.js
@@ -15,6 +15,6 @@ router.post('/:roomId/start', checkUser, require('./roomStartPOST'));
 router.post('/:roomId/status', checkUser, require('./roomStatusPOST'));
 router.post('/:roomId/spark', checkUser, require('./sparkPOST'));
 router.post('/:roomId/record', checkUser, uploadImageIntoSubDir('certification'), require('./roomRecordPOST'));
-router.post('/:roomId/out', checkUser, require('./roomOutPOST'));
+router.delete('/:roomId/out', checkUser, require('./roomOutPOST'));
 
 module.exports = router;
