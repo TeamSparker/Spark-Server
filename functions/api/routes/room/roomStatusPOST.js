8bf2a12aa549e0bc7dea76d4288555083af19515
youngkwon02 <youngkwon02@naver.com>
2022-02-26 18:24:46 +0900

    refactor: 고민중 API FCM multicast 및 그룹화 적용

--- functions/api/routes/room/roomStatusPOST.js
+++ functions/api/routes/room/roomStatusPOST.js
@@ -91,17 +91,19 @@ module.exports = async (req, res) => {
         return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.CONSIDER_ALREADY_DONE));
       }
       const sender = await userDB.getUserById(client, userId);
-      const receivers = await roomDB.getFriendsByIds(client, roomId, userId);
-      const receiversIds = receivers.map((r) => r.userId);
+      const friends = await roomDB.getFriendsByIds(client, roomId, userId);
+      const receiverTokens = friends.map((f) => f.deviceToken);
       const { title, body, isService } = alarmMessage.STATUS_CONSIDERING(sender.nickname, room.roomName);
 
-      for (let i = 0; i < receiversIds.length; i++) {
-        const receiverId = receiversIds[i];
-        const receiver = await userDB.getUserById(client, receiverId);
-        const receiverToken = receiver.deviceToken;
-        await noticeDB.addNotification(client, title, body, sender.profileImg, receiverId, isService);
-        pushAlarm.send(req, res, receiverToken, title, body);
+      const notifications = friends.map((f) => {
+        return `('${title}', '${body}', 'Spark_IMG_URL', ${f.userId}, ${isService})`;
+      });
+
+      if (notifications.length) {
+        await noticeDB.addNotifications(client, notifications);
       }
+
+      pushAlarm.sendMulticastByTokens(req, res, title, body, receiverTokens, 'consider');
     }
 
     return res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.UPDATE_STATUS_SUCCESS));
