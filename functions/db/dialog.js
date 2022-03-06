c7c0712fe9c63b755ce2bdad5a2e5bf10204d082
xxeol2 <jhj01177@naver.com>
2022-03-06 21:42:34 +0900

    feat: 습관방 조회 리스트 다이얼로그 추가

--- functions/db/dialog.js
+++ functions/db/dialog.js
@@ -17,11 +17,25 @@ const insertDialogs = async (client, dialogs) => {
   return convertSnakeToCamel.keysToCamel(rows);
 };
 
+const getUserDialogs = async (client, userId, types) => {
+  const { rows } = await client.query(
+    `
+      SELECT * 
+      FROM spark.dialog 
+      WHERE user_id = $1
+      AND type IN (${types.join(',')})
+      AND is_read = FALSE
+    `,
+    [userId],
+  );
+  return convertSnakeToCamel.keysToCamel(rows);
+};
+
 const insertLifeDeductionDialogs = async (client, dialogs) => {
   const { rows } = await client.query(
     `
     INSERT INTO spark.dialog
-    (user_id, room_id, life_deduction_count)
+    (user_id, room_id, life_deduction_count, type)
     VALUES
     ${dialogs.join(',')}
     RETURNING *
@@ -34,5 +48,6 @@ const insertLifeDeductionDialogs = async (client, dialogs) => {
 
 module.exports = {
   insertLifeDeductionDialogs,
-    insertDialogs
+    insertDialogs,
+    getUserDialogs,
 };
--- functions/db/index.js
+++ functions/db/index.js
@@ -7,5 +7,5 @@ module.exports = {
   recordDB: require('./record'),
   scheduleDB: require('./schedule'),
   reportDB: require('./report'),
-  dialogDB: require('./dialg'),
+  dialogDB: require('./dialog'),
 };
