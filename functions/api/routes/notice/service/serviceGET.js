fd9f2d112e92ae2ff6c40b366ed791cdaca57087
youngkwon02 <youngkwon02@naver.com>
2022-03-08 15:45:25 +0900

    feat: serviceGET slackAPI 연동

--- functions/api/routes/notice/service/serviceGET.js
+++ functions/api/routes/notice/service/serviceGET.js
@@ -54,7 +54,7 @@ module.exports = async (req, res) => {
     console.log(error);
 
     const slackMessage = `[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl} ${error} ${JSON.stringify(error)}`;
-    // slackAPI.sendMessageToSlack(slackMessage, slackAPI.DEV_WEB_HOOK_ERROR_MONITORING);
+    slackAPI.sendMessageToSlack(slackMessage, slackAPI.DEV_WEB_HOOK_ERROR_MONITORING);
     res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR));
   } finally {
     client.release();
