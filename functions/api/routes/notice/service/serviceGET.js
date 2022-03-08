7ea5c0eb50040d51ea948ecfffcd5150a37f20a1
youngkwon02 <youngkwon02@naver.com>
2022-03-08 15:35:15 +0900

    refactor: serviceGET notices body 수정

--- functions/api/routes/notice/service/serviceGET.js
+++ functions/api/routes/notice/service/serviceGET.js
@@ -1,8 +1,10 @@
 const functions = require('firebase-functions');
+const dayjs = require('dayjs');
 const util = require('../../../../lib/util');
 const statusCode = require('../../../../constants/statusCode');
 const responseMessage = require('../../../../constants/responseMessage');
 const db = require('../../../../db/db');
+const slackAPI = require('../../../../middlewares/slackAPI');
 const { noticeDB } = require('../../../../db');
 
 /**
@@ -22,14 +24,23 @@ module.exports = async (req, res) => {
     client = await db.connect(req);
 
     const services = await noticeDB.getServicesByUserId(client, userId, parseInt(lastId), parseInt(size));
+    let now = dayjs().add(9, 'hour');
+    now = now.set('hour', 0);
+    now = now.set('minute', 0);
+    now = now.set('second', 1);
 
     const notices = services.map((s) => {
       const notice = {};
+      let createdAt = dayjs(s.createdAt);
+      createdAt = createdAt.set('hour', 0);
+      createdAt = createdAt.set('minute', 0);
+      createdAt = createdAt.set('second', 0);
+
       notice['noticeId'] = s.notificationId;
       notice['noticeTitle'] = s.title;
-      notice['noticeImg'] = s.thumbnail;
       notice['noticeContent'] = s.content;
-      notice['createdAt'] = s.createdAt.toISOString().split('T')[0].replace(/-/g, '/');
+      notice['day'] = now.diff(createdAt, 'd') > 0 ? `${passedDay}일전` : `오늘`;
+      notice['isNew'] = !s.isRead;
       return notice;
     });
 
@@ -37,6 +48,9 @@ module.exports = async (req, res) => {
   } catch (error) {
     functions.logger.error(`[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`, `[CONTENT] ${error}`);
     console.log(error);
+
+    const slackMessage = `[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl} ${error} ${JSON.stringify(error)}`;
+    // slackAPI.sendMessageToSlack(slackMessage, slackAPI.DEV_WEB_HOOK_ERROR_MONITORING);
     res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR));
   } finally {
     client.release();
