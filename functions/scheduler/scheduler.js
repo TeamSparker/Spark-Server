2268123c75da38b77bc4dfe6e4364d914ca9f836
youngkwon02 <youngkwon02@naver.com>
2022-03-23 00:31:40 +0900

    HOTFIX: scheduling time setting 수정

--- functions/scheduler/scheduler.js
+++ functions/scheduler/scheduler.js
@@ -3,15 +3,12 @@ const schedule = require('node-schedule');
 const funcs = require('./funcs.js');
 
 const jobSchedule = schedule.scheduleJob('0 0 15 * * *', function () {
-  const now = dayjs();
-  if (now.get('h') === 0 && now.get('m') === 0) {
-    funcs.checkLife();
-  }
+  funcs.checkLife();
 });
 
-const remindSchedule = schedule.scheduleJob('0 0 21 * * *', function () {
+const remindSchedule = schedule.scheduleJob('0 0 12 * * *', function () {
   const now = dayjs();
-  if (now.get('h') === 21 && now.get('m') === 0) {
+  if (now.get('h') === 12 && now.get('m') === 0) {
     funcs.sendRemind();
   }
 });
