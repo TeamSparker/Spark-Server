75804c1fe4f9e0805b86d0e412adffe48a74d5a5
youngkwon02 <youngkwon02@naver.com>
2022-03-24 02:26:52 +0900

    chore: /auth/signout router connection

--- functions/api/routes/auth/index.js
+++ functions/api/routes/auth/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const uploadImageIntoSubDir = require('../../../middlewares/uploadImage');
 
 router.post('/signup', uploadImageIntoSubDir('users'), require('./authSignUpPOST'));
+router.post('/signout', require('./authSignOutPOST'));
 router.get('/doorbell', require('./authDoorbellGET'));
 
 module.exports = router;
