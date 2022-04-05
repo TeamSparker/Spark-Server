5e54b22bb1d8a467cb6b3904dd30ca1bce4c8f85
youngkwon02 <youngkwon02@naver.com>
2022-04-05 15:07:29 +0900

    refactor: deleteImageFromFirebase func parameter

--- functions/lib/deleteImage.js
+++ functions/lib/deleteImage.js
@@ -2,11 +2,7 @@ const admin = require('firebase-admin');
 const { firebaseConfig } = require('../config/firebaseClient');
 
 const deleteImageFromFirebase = async (filePath) => {
-  await admin
-    .storage()
-    .bucket(firebaseConfig.storageBucket)
-    .file('certification/' + filePath)
-    .delete();
+  await admin.storage().bucket(firebaseConfig.storageBucket).file(filePath).delete();
 };
 
 module.exports = {
