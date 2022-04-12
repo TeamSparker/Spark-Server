9b1d2e970dd6433519c2ce5a6b722c4ea88b8b7b
youngkwon02 <youngkwon02@naver.com>
2022-04-12 15:05:53 +0900

    feat: lib/deleteFirebaseImageByUserId

--- functions/lib/deleteImage.js
+++ functions/lib/deleteImage.js
@@ -1,10 +1,19 @@
 const admin = require('firebase-admin');
 const { firebaseConfig } = require('../config/firebaseClient');
+const { ownershipDB } = require('../db');
 
-const deleteImageFromFirebase = async (filePath) => {
+const deleteFirebaseImage = async (filePath) => {
   await admin.storage().bucket(firebaseConfig.storageBucket).file(filePath).delete();
 };
 
+const deleteFirebaseImageByUserId = async (client, userId) => {
+  const ownership = await ownershipDB.getOwnershipByUserId(client, userId);
+  for (let i = 0; i < ownership.length; i++) {
+    deleteFirebaseImage(ownership[i].filePath);
+  }
+};
+
 module.exports = {
-  deleteImageFromFirebase,
+  deleteFirebaseImage,
+  deleteFirebaseImageByUserId,
 };
