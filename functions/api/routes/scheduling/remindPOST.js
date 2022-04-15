e45612673c5b8440fe8a111f00ccd32d0d9ea3fe
youngkwon02 <youngkwon02@naver.com>
2022-04-15 13:19:27 +0900

    comment: update remindPOST endpoint comment

---
+++ functions/api/routes/scheduling/remindPOST.js
@@ -0,0 +1,27 @@
+const functions = require('firebase-functions');
+const util = require('../../../lib/util');
+const statusCode = require('../../../constants/statusCode');
+const responseMessage = require('../../../constants/responseMessage');
+const slackAPI = require('../../../middlewares/slackAPI');
+const { sendRemind } = require('../../../scheduler/funcs');
+
+/**
+ *  @리마인드_푸시알림_전송
+ *  @route POST /scheduling/remind
+ *  @error
+ */
+
+module.exports = async (req, res) => {
+  try {
+    // 리마인드 푸시알림 전송
+    sendRemind();
+    return res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.SEND_REMIND_SUCCESS));
+  } catch (error) {
+    console.log(error);
+    functions.logger.error(`[SEND REMIND ERROR] [${req.method.toUpperCase()}]`);
+    const slackMessage = `[SEND REMIND ERROR] [${req.method.toUpperCase()}]`;
+    slackAPI.sendMessageToSlack(slackMessage, slackAPI.DEV_WEB_HOOK_ERROR_MONITORING);
+    res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR));
+  } finally {
+  }
+};
