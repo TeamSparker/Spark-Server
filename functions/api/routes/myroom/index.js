500b86abcda46cbab5566a0f96847fc9230b63fd
youngkwon02 <youngkwon02@naver.com>
2022-04-28 15:13:04 +0900

    feat: myroomThumbnailListGET API implemented

--- functions/api/routes/myroom/index.js
+++ functions/api/routes/myroom/index.js
@@ -4,6 +4,7 @@ const { checkUser } = require('../../../middlewares/auth');
 
 router.get('/:roomId', checkUser, require('./myroomRoomGET'));
 router.get('', checkUser, require('./myroomGET'));
+router.get('/thumbnail/:roomId', checkUser, require('./myroomThumbnailListGET'));
 router.patch('/:roomId/thumbnail/:recordId', checkUser, require('./myroomThumbnailPATCH'));
 
 module.exports = router;
