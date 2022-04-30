5cf72b84a23183cadb8eff29d690626eb556a8b8
youngkwon02 <youngkwon02@naver.com>
2022-04-30 13:13:37 +0900

    feat: versionGET API implemented

--- functions/api/routes/version/versionGET.js
+++ functions/api/routes/version/versionGET.js
@@ -3,6 +3,7 @@ const util = require('../../../lib/util');
 const statusCode = require('../../../constants/statusCode');
 const responseMessage = require('../../../constants/responseMessage');
 const db = require('../../../db/db');
+const { versionDB } = require('../../../db');
 
 /**
  *  @릴리즈_버전정보_조회
@@ -16,10 +17,11 @@ module.exports = async (req, res) => {
   try {
     client = await db.connect(req);
 
-    // return res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.GET_USER_PROFILE_SUCCESS, { nickname, profileImg }));
+    const { version } = await versionDB.getRecentVersion(client);
+
+    return res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.GET_RECENT_VERSION_SUCCESS, { version }));
   } catch (error) {
     functions.logger.error(`[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`, `[CONTENT] ${error}`);
-    console.log(error);
 
     return res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR));
   } finally {
