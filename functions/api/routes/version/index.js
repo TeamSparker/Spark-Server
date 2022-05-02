173fc8e84682805fd3b6d69793cabcfea51046a0
youngkwon02 <youngkwon02@naver.com>
2022-05-03 02:58:51 +0900

    feat: versionPATCH API implemented

--- functions/api/routes/version/index.js
+++ functions/api/routes/version/index.js
@@ -2,5 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 router.get('', require('./versionGET'));
+router.patch('', require('./versionPATCH'));
 
 module.exports = router;
