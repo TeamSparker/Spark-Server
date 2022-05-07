471279b65664ebd9a202c445d9747f63c21cd6f1
youngkwon02 <youngkwon02@naver.com>
2022-05-07 23:38:21 +0900

    chore: setup dbConfig according to dev, production

--- functions/config/dbConfig.js
+++ functions/config/dbConfig.js
@@ -1,9 +1,21 @@
 const dotenv = require('dotenv');
 dotenv.config();
 
+let user = process.env.DEV_DB_USER;
+let host = process.env.DEV_DB_HOST;
+let database = process.env.DEV_DB_DB;
+let password = process.env.DEV_DB_PASSWORD;
+
+if (process.env.NODE_ENV === 'production') {
+  user = process.env.DB_USER;
+  host = process.env.DB_HOST;
+  database = process.env.DB_DB;
+  password = process.env.DB_PASSWORD;
+}
+
 module.exports = {
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DB,
-  password: process.env.DB_PASSWORD,
+  user,
+  host,
+  database,
+  password,
 };
