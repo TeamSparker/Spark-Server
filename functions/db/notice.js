5095173c4fcdcbb0ab9d90ce04684dde0f91f71d
youngkwon02 <youngkwon02@naver.com>
2022-05-27 16:42:50 +0900

    fix: noticeDB.getActivesByUserId UTC+9 handling

--- functions/db/notice.js
+++ functions/db/notice.js
@@ -145,7 +145,7 @@ const getActivesByUserId = async (client, userId, lastId, size) => {
         AND is_deleted = FALSE
         AND is_service = FALSE
         AND notification_id < $2
-        AND created_at >= CURRENT_DATE - INTERVAL '7 days'
+        AND created_at >= (CURRENT_TIMESTAMP - INTERVAL '7 days - 9 hours')::date
         ORDER BY notification_id DESC
         LIMIT $3
       `,
