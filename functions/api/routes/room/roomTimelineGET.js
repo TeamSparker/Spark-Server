0fa1db2e4532e85c072824d9959f1515ef6c79ce
youngkwon02 <youngkwon02@naver.com>
2022-09-26 15:27:25 +0900

    feat: 생명 타임라인 조회 API 구현

---
+++ functions/api/routes/room/roomTimelineGET.js
@@ -0,0 +1,86 @@
+const functions = require('firebase-functions');
+const util = require('../../../lib/util');
+const statusCode = require('../../../constants/statusCode');
+const responseMessage = require('../../../constants/responseMessage');
+const db = require('../../../db/db');
+const { roomDB, lifeTimelineDB } = require('../../../db');
+const timelineMessage = require('../../../constants/lifeTimelineMessage');
+const slackAPI = require('../../../middlewares/slackAPI');
+const dayjs = require('dayjs');
+
+/**
+ *  @습관방_생명_타임라인_조회
+ *  @route GET /room/:roomId/timeline
+ *  @error
+ *      1. 존재하지 않는 습관방인 경우
+ *      2. 진행중인 습관방이 아닌 경우
+ *      3. 접근 권한이 없는 유저인 경우
+ */
+
+module.exports = async (req, res) => {
+  const { roomId } = req.params;
+  const user = req.user;
+
+  let client;
+
+  try {
+    client = await db.connect(req);
+
+    const room = await roomDB.getRoomById(client, roomId);
+
+    // @error 1. 존재하지 않는 습관방인 경우
+    if (!room) {
+      res.status(statusCode.NO_CONTENT).send(util.fail(statusCode.NO_CONTENT, responseMessage.GET_ROOM_DATA_FAIL));
+    }
+
+    const endDate = dayjs(room.endAt);
+    const now = dayjs().add(9, 'hour');
+    const today = dayjs(now.format('YYYY-MM-DD'));
+    const leftDay = endDate.diff(today, 'day');
+
+    // @error 2. 진행중인 습관방이 아닌 경우
+    if (room.status !== 'ONGOING' || leftDay < 0) {
+      res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.NOT_ONGOING_ROOM));
+    }
+
+    const entries = await roomDB.getEntriesByRoomId(client, roomId);
+    const userEntry = entries.filter((entry) => entry.userId === user.userId);
+    // @error 3. 접근 권한이 없는 유저인 경우
+    if (!userEntry.length) {
+      res.status(statusCode.UNAUTHORIZED).send(util.fail(statusCode.UNAUTHORIZED, responseMessage.NOT_MEMBER));
+    }
+
+    let timelines = await lifeTimelineDB.getLifeTimeline(client, roomId, user.userId);
+
+    timelines = timelines.map((t) => {
+      const timeline = {};
+      const { title, content } = timelineMessage.LIFE_DECREASE(t.decreaseCount);
+
+      let createdAt = dayjs(t.createdAt);
+      const passedDay = now.diff(createdAt, 'd');
+      const profiles = [t.profile_1, t.profile_2].filter((profile) => profile !== null);
+
+      timeline['title'] = title;
+      timeline['content'] = content;
+      timeline['profiles'] = profiles;
+      timeline['day'] = passedDay > 0 ? `${passedDay}일전` : `오늘`;
+      timeline['isNew'] = !t.isRead;
+
+      return timeline;
+    });
+
+    return res.status(statusCode.OK).send(
+      util.success(statusCode.OK, responseMessage.GET_LIFE_TIMELINE_SUCCESS, {
+        timelines,
+      }),
+    );
+  } catch (error) {
+    console.log(error);
+    functions.logger.error(`[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`, `[CONTENT] ${error}`);
+    const slackMessage = `[ERROR BY ${user.nickname} (${user.userId})] [${req.method.toUpperCase()}] ${req.originalUrl} ${error} ${JSON.stringify(error)}`;
+    slackAPI.sendMessageToSlack(slackMessage, slackAPI.DEV_WEB_HOOK_ERROR_MONITORING);
+    return res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR));
+  } finally {
+    client.release();
+  }
+};
