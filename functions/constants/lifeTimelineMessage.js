173501b108b6581089dfbd3fa060988ea7360829
youngkwon02 <youngkwon02@naver.com>
2022-10-01 18:02:45 +0900

    chore: LIFE_FILL 응답 메시지 추가

--- functions/constants/lifeTimelineMessage.js
+++ functions/constants/lifeTimelineMessage.js
@@ -4,6 +4,26 @@ const LIFE_DECREASE = (count) => {
   return { title, content };
 };
 
+const LIFE_FILL = (termDay) => {
+  const title = `생명 충전 완료🔋`;
+  let content = '';
+
+  if (termDay === 1) {
+    content = '66일의 도전을 시작했네요. 인증하고 생명을 지켜요!';
+  } else if (termDay === 4) {
+    content = '3일 달성 선물로 생명이 충전됐어요. 잘하고 있어요!';
+  } else if (termDay === 8) {
+    content = '7일 달성 선물로 생명이 충전됐어요. 더 힘내봐요!';
+  } else if (termDay === 34) {
+    content = '33일 달성 선물로 생명이 충전됐어요. 할 수 있어요!';
+  } else if (termDay === 60) {
+    content = '마지막 7일 선물로 생명이 충전됐어요. 끝까지 힘내요!';
+  }
+
+  return { title, content };
+};
+
 module.exports = {
   LIFE_DECREASE,
+  LIFE_FILL,
 };
